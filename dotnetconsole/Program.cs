81857f0daad76fd56996218f2c27aeaec747f709
Tri atmoko <triatmoko95@gmail.com>
2023-04-05 09:24:42 +0700

    ubah hello wrorld 3 jadi 5

--- dotnetconsole/Program.cs
+++ dotnetconsole/Program.cs
@@ -8,5 +8,5 @@ Console.WriteLine("Hello, World 1");
 //tambah hello world 3
 Console.WriteLine("Hello, World 2");
 
-//tambah hello world 3
-Console.WriteLine("Hello, World 3");
+//ubah hello world 3 jadi 5
+Console.WriteLine("Hello, World 5");
